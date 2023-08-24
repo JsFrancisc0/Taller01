9894e821cb1ab60cec05aeea7aa9e5269d81827c
JsFrancisc0 <j.saldias03@ufromail.cl>
2023-08-24 15:42:21 -0400

    creacion de metodos

---
+++ Cartas.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Cartas {
+
+    public static void main(String[] args){
+
+        do while{
+
+        } (continuar=true)
+
+    }
+
+    public int crearMatrizCartas(){
+
+
+
+    }
+    public int agregarCartas(String nombreCarta, String puntaje){
+
+    }
+
+    public void continuarJuego(){
+
+        private Scanner = new Scanner(System.in);
+        char respuesta;
+        do{
+            System.out.println("Desea Segir Jugando? [s][n]");
+
+
+        } while (respuesta
+
+    } return continuar;
+}
